cebcf3a6e1b187100b632798fb5dcacfcdd9b05a
MarvJungs <Marvin10072000@live.de>
2023-11-15 17:43:37 +0100

    Moved DB-Config data to constants

--- _includes/includes.php
+++ _includes/includes.php
@@ -2,6 +2,5 @@
 
 include($_SERVER['DOCUMENT_ROOT'].'/_includes/functions.php');
 include($_SERVER['DOCUMENT_ROOT'].'/_includes/db.php');
-include($_SERVER['DOCUMENT_ROOT'].'/_includes/config.php');
 
 ?>
