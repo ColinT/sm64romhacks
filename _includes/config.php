df8130a30c4189543090c04128189edd11b6d638
MarvJungs <Marvin10072000@live.de>
2023-11-15 01:22:08 +0100

    Moved important functions/constants into own files

---
+++ _includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("ADMIN_NEWS", array("107568694359531520"));
+define("ADMIN_SITE", array("107568694359531520"));
+
+
+
+?>
---
+++ _includes/includes.php
@@ -0,0 +1,7 @@
+<?php
+
+include($_SERVER['DOCUMENT_ROOT'].'/_includes/functions.php');
+include($_SERVER['DOCUMENT_ROOT'].'/_includes/db.php');
+include($_SERVER['DOCUMENT_ROOT'].'/_includes/config.php');
+
+?>
