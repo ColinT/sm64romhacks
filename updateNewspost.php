32b6326c572eb86ac00a2b1dcc19d4b0e1d80723
MarvJungs <Marvin10072000@live.de>
2023-11-16 21:33:37 +0100

    Made news page to the main page

---
+++ updateNewspost.php
@@ -0,0 +1,20 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'].'/_includes/includes.php';
+
+
+
+
+if(!isset($_POST['post_id'])) {
+    header("Location: /login/error.php");
+    die();
+}
+$post_title = stripChars($_POST['post_title']);
+$post_text = stripChars($_POST['post_text']);
+$post_id = intval($_POST['post_id']);
+
+updateNewspostInDatabase($pdo,$post_id,$post_title,$post_text);
+
+header("Location: /news");
+die();
+?>
