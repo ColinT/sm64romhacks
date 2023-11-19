66277372b9005569647ca9e72c5a3fb4c28c06db
MarvJungs <Marvin10072000@live.de>
2023-11-19 14:06:10 +0100

    Modified de/encoding functions

--- _includes/functions.php
+++ _includes/functions.php
@@ -8,30 +8,18 @@ function stripChars($string) {
 
 function getURLEncodedName($hackname)
 {
-	/*$hackname=str_replace(" ","%20",$hackname);
-	$hackname=str_replace("(","%28",$hackname);
-	$hackname=str_replace(")","%29",$hackname);
-	$hackname=str_replace("'","%27",$hackname);
-	$hackname=str_replace(":","%3A",$hackname);
-    $hackname=str_replace(".","%2E",$hackname);
-	$hackname=str_replace("/","%2F",$hackname);
-    $hackname=str_replace("!","%21",$hackname);
-	$hackname=str_replace("?","%3F",$hackname);*/
-
-	//$hackname=strtolower($hackname);
+	
 	$hackname=str_replace(":","_",$hackname);
 
-	return $hackname;
-	//return filter_var($hackname, FILTER_SANITIZE_URL);
+	return urlencode($hackname);
 }
 
 function getURLDecodedName($hackname)
 {
 	$hackname=str_replace("_",":",$hackname);
-	$hackname=str_replace("%20"," ",$hackname);
 
 
-	return $hackname;
+	return urldecode($hackname);
 }
 
 
