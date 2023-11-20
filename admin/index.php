02b1afba21a882b9fff1c31b367df664864650d3
MarvJungs <Marvin10072000@live.de>
2023-11-21 00:29:54 +0100

    Moved SVGs to external files

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,8 @@ if(!$_SESSION['logged_in'] || !in_array($_SESSION['userData']['discord_id'], ADM
 	die();
 }
 
-$data = getClaimsFromDatabase($pdo);
+$claims = getClaimsFromDatabase($pdo);
+$pending_hacks = getAllPendingHacksFromDatabase($pdo);
 ?>
 
 <!DOCTYPE HTML>
@@ -31,11 +32,18 @@ $data = getClaimsFromDatabase($pdo);
 				<!--HTML CONTENT HERE-->
 				<table class="table-sm table-bordered">
 					<tr><th>Claim ID</th><th>Hack ID</th><th>User ID</th><th>Claimed Author</th><th class="border-0">&nbsp;</th></tr>
-					<?php foreach($data as $entry) { ?>
-					<tr><td><?php print($entry['claim_id']);?></td><td><?php print($entry['hack_id']);?></td><td><?php print($entry['user_id']);?></td><td><?php print($entry['claimed_author']);?></td><td class="border-0"><a class="btn btn-success text-nowrap" href="acceptClaim.php?claim_id=<?php print($entry['claim_id']);?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd"></path></svg></a>&nbsp;<a class="btn btn-danger text-nowrap" href="rejectClaim.php?claim_id=<?php print($entry['claim_id']);?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></a></td></tr>	
+					<?php foreach($claims as $entry) { ?>
+					<tr><td><?php print($entry['claim_id']);?></td><td><?php print($entry['hack_id']);?></td><td><?php print($entry['user_id']);?></td><td><?php print($entry['claimed_author']);?></td><td class="border-0"><a class="btn btn-success text-nowrap" href="acceptClaim.php?claim_id=<?php print($entry['claim_id']);?>"><img src="/_img/accept.svg"></a>&nbsp;<a class="btn btn-danger text-nowrap" href="rejectClaim.php?claim_id=<?php print($entry['claim_id']);?>"><img src="/_img/delete.svg"></a></td></tr>	
 					<?php } ?>
 					</table>
 					</div>
+				<div class="table-responsive">
+				<table class="table-sm table-bordered">
+					<tr><th>Hack Name</th><th>Version</th><th>Hack Author</th><th>Star Count</th><th>Release Date</th><th>Tag</th><th>Description</th></tr>
+					<?php foreach($pending_hacks as $entry) { ?>
+					<tr><td><?php print($entry['hack_name']);?></td><td><?php print($entry['hack_version']);?></td><td><?php print($entry['hack_author']);?></td><td><?php print($entry['hack_starcount']);?></td><td><?php print($entry['hack_release_date']);?></td><td><?php print($entry['hack_tags']);?></td><td><?php print($entry['hack_description']);?></td><td class="border-0"><a class="btn btn-success text-nowrap" href="acceptClaim.php?claim_id=<?php print($entry['claim_id']);?>"><img src="/_img/accept.svg"></a>&nbsp;<a class="btn btn-danger text-nowrap" href="rejectClaim.php?claim_id=<?php print($entry['claim_id']);?>"><img src="/_img/delete.svg"></a></td></tr>	
+					<?php } ?>
+
 			<?php include($_SERVER['DOCUMENT_ROOT'].'/_includes/footer.php'); ?>	
 			</div>		</div>
 	</body>
