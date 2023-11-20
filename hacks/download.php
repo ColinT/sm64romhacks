281b954b45696a90aba9606634e6e9fcc8ccb2d0
MarvJungs <Marvin10072000@live.de>
2023-11-20 15:19:58 +0100

    Bot check

--- hacks/download.php
+++ hacks/download.php
@@ -4,10 +4,12 @@ include($_SERVER['DOCUMENT_ROOT'] . '/_includes/includes.php');
 
 $hack_id = $_GET['hack_id'];
 
-updateDownloadCounter($pdo, $hack_id);
+if(!is_bot()) {
+    updateDownloadCounter($pdo, $hack_id);
 
-$patchname = getPatchFromDatabase($pdo, $hack_id)[0]['hack_patchname'];
-header("Location: /patch/" . $patchname . ".zip");
-die();
+    $patchname = getPatchFromDatabase($pdo, $hack_id)[0]['hack_patchname'];
+    header("Location: /patch/" . $patchname . ".zip");
+    die();    
+}
 
 ?>
