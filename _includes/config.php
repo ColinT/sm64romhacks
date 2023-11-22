9a84b376a9755c04b8196418a2c5a03184bd43ec
MarvJungs <Marvin10072000@live.de>
2023-11-22 15:46:09 +0100

    Update config.php

--- _includes/config.php
+++ _includes/config.php
@@ -8,13 +8,11 @@ define("DB_USER", "s294518_2992945");
 define("DB_PASS", "erSzyhRFG6Uebjd()");
 define("DB_NAME", "db294518x2992945");
 
-//define("DISCORD_CLIENT_ID", "1171219678132195419");
 define("DISCORD_CLIENT_ID", "1176646640321441802");
-//define("DISCORD_CLIENT_SECRET", "vKrWpROs_jnIdVycxGS9nTPpVBpZydJT");
 define("DISCORD_CLIENT_SECRET", "u5_sAUYf9d9w66ZZX2h1krOTUxtKiKol");
 define("DISCORD_REDIRECT_URI", "http://localhost/login/process-oauth.php");
 
-define("TWITCH_CLIENT_ID", "7xxy8kh0hvswrrnnpzjpej41h5qxzz");
-define("TWITCH_CLIENT_SECRET", "26i3l38b88g131htepkjbn8t9rolon");
+define("TWITCH_CLIENT_ID", "YOUR_TWITCH_CLIENT_ID");
+define("TWITCH_CLIENT_SECRET", "YOUR_TWITCH_CLIENT_SECRET");
 
 ?>
