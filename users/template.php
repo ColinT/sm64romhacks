f027e7be7d0d877c5f10a0808fa55c0003386e46
MarvJungs <Marvin10072000@live.de>
2023-11-28 01:38:39 +0100

    User links

--- users/template.php
+++ users/template.php
@@ -14,17 +14,33 @@
 		<body>		<div class="container">
 		<?php include($_SERVER['DOCUMENT_ROOT'].'/_includes/header.php'); ?>
 				<div align="center">
+					<h1><img src="https://cdn.discordapp.com/avatars/<?php print($user['discord_id']); ?>/<?php print($user['discord_avatar']);?>.jpg" height=64 width=64><?php print($user['discord_username']);?></h1><hr/>
 					<!--HTML CONTENT HERE-->
-					
+					<?php if(sizeof($data) == 0) { ?>
+						This user has no published ROM Hacks!
+			<?php } 
+			else { ?>
+
 					<div class="table-responsive">
 		        	<table class="table-sm table-bordered">
 			        <tr><th><b>Hack Name</b></th><th><b>Creator</b></th><th>Release Date</th></tr>
 					<?php foreach($data as $entry) { 
-						$authors = getAllAuthorsNames($pdo, $entry['author']);?> 
+						//$authors = getAllAuthorsNames($pdo, $entry['author']); 
+						$authors = $entry['author'];
+						$authors = explode(", ", $authors);
+						$hack_author = "";
+						foreach($authors as $author) {
+						  $user = getUserFromDatabase($pdo, $author);
+						  if($user) $hack_author = $hack_author . '<a href="/users/' . $author . '">' . $user['discord_username'] . '</a>, ';
+						  else $hack_author = $hack_author . $author . ', ';
+						}
+						$hack_author = substr_replace($hack_author, '', -2); ?>
+
 						
-						<tr><td><a href="/hacks/<?php print(getURLEncodedName($entry['hack_name']));?>"><?php print($entry['hack_name']); ?></a></td><td><?php print($authors); ?></td><td><?php print($entry['release_date']); ?></td></tr>
+						<tr><td><a href="/hacks/<?php print(getURLEncodedName($entry['hack_name']));?>"><?php print($entry['hack_name']); ?></a></td><td><?php print($hack_author); ?></td><td><?php print($entry['release_date']); ?></td></tr>
 					<?php } ?>
 			    </table></div> <br/>
+				<?php } ?>
 
 			<?php include($_SERVER['DOCUMENT_ROOT'].'/_includes/footer.php'); ?>
 				</div>		</div>
