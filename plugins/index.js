607c68e60384d27df8c224803f508e2e47e19672
MarvJungs <Marvin10072000@live.de>
2023-12-10 21:27:34 +0100

    Fixed collapsibles not working

--- plugins/index.js
+++ plugins/index.js
@@ -4,6 +4,21 @@ function main()
 {
   document.getElementById("windows").addEventListener("click", showWindows);
   document.getElementById("linux").addEventListener("click", showLinux);
+
+  var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
 }
 
 function showWindows()
