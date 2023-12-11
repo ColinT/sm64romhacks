50e56a5c835cc15d5972e00a19c2c13a82d4a084
MarvJungs <Marvin10072000@live.de>
2023-12-11 21:58:43 +0100

    Updated for testing

--- _includes/config.php
+++ _includes/config.php
@@ -1,19 +1,20 @@
 <?php
 
 define("ADMIN_NEWS", array("107568694359531520"));
-define("ADMIN_SITE", array("107568694359531520"));
+//                          AndrewSM64              FrostyZako              MarvJungs               Mushie64               Tomatobird8
+define("ADMIN_SITE", array("260945217664974860", "120084264489451520", "107568694359531520", "264457080889409536", "210326854282772480"));
 
-define("DB_HOST", "localhost");
-define("DB_USER", "db");
-define("DB_PASS", "db");
-define("DB_NAME", "new_application");
+define("DB_HOST", "mysql33.1blu.de");
+define("DB_USER", "s294518_3540787");
+define("DB_PASS", "tGTHqY!f2!L@s(j");
+define("DB_NAME", "db294518x3540787");
 
 define("DISCORD_CLIENT_ID", "1176646640321441802");
 define("DISCORD_CLIENT_SECRET", "u5_sAUYf9d9w66ZZX2h1krOTUxtKiKol");
-define("DISCORD_REDIRECT_URI", "http://localhost/login/process-oauth.php");
+define("DISCORD_REDIRECT_URI", "https://test.sm64romhacks.com/login/process-oauth.php");
 
-define("TWITCH_CLIENT_ID", "YOUR_TWITCH_CLIENT_ID");
-define("TWITCH_CLIENT_SECRET", "YOUR_TWITCH_CLIENT_SECRET");
+define("TWITCH_CLIENT_ID", "7xxy8kh0hvswrrnnpzjpej41h5qxzz");
+define("TWITCH_CLIENT_SECRET", "26i3l38b88g131htepkjbn8t9rolon");
 
 error_reporting(E_ERROR);
 
