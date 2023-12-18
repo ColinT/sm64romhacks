0e2864da8ee543ee0d467381ba4e4ccd7321455e
MarvJungs <Marvin10072000@live.de>
2023-12-18 15:47:47 +0100

    download counter doesnt increase on failure anymore

--- hacks/download.php
+++ hacks/download.php
@@ -5,9 +5,12 @@ include($_SERVER['DOCUMENT_ROOT'] . '/_includes/includes.php');
 $hack_id = intval($_GET['hack_id']);
 
 if(!is_bot()) {
-    updateDownloadCounter($pdo, $hack_id);
 
     $patchname = getPatchFromDatabase($pdo, $hack_id)[0]['hack_patchname'];
+
+    if(file_exists($_SERVER['DOCUMENT_ROOT'] . "/patch/$patchname.zip"))     updateDownloadCounter($pdo, $hack_id);
+
+
     header("Location: /patch/" . $patchname . ".zip");
     die();    
 }
