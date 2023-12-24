6295f87bb5eb6b52ceb00ec86d21a46c3893428f
MarvJungs <Marvin10072000@live.de>
2023-12-24 18:23:57 +0100

    disclaimers

--- codes/index.php
+++ codes/index.php
@@ -3,6 +3,7 @@
 include $_SERVER['DOCUMENT_ROOT'].'/_includes/includes.php';
 
 ?>
+
 <!DOCTYPE HTML>
 <html>
 <!--BEGINNING OF HEAD-->
@@ -12,8 +13,10 @@ include $_SERVER['DOCUMENT_ROOT'].'/_includes/includes.php';
 <meta name="keywords" content="super mario, romhacks, hack, speedrun, sm64hacks, sm64romhacks, rom, modification" />
 <meta name="description" content="Welcome to SM64ROMHacks! We have a really big collection of SM64 ROM Hacks which wait to be played! Community News/Events will also be tracked here" />
 <link rel="stylesheet" type="text/css" href="/_assets/_css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../_css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 <link rel="shortcut icon" href="/_assets/_img/icon.ico" />
+<link rel="shortcut icon" href="../_img/icon.ico" />
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
 
@@ -205,7 +208,8 @@ include $_SERVER['DOCUMENT_ROOT'].'/_includes/includes.php';
 			D033AFA1 0020<br/>
 			8133B17E 0880<br/>
 			</code>
-		</div>
+		</div><hr/><br/>
+		Codes taken from: <a href="https://sites.google.com/view/supermario64/code">https://sites.google.com/view/supermario64/code</a><br/><br/>
 		<?php include($_SERVER['DOCUMENT_ROOT'].'/_includes/footer.php'); ?>
 	</div>
 </body>	
