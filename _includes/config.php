8c503d696a184a3b0a5a042ca62d43d0159e4552
MarvJungs <Marvin10072000@live.de>
2023-12-31 13:12:45 +0100

    Update config.php

--- _includes/config.php
+++ _includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("ADMIN_NEWS", array("260945217664974860", "120084264489451520", "107568694359531520", "264457080889409536", "210326854282772480"));
+define("ADMIN_NEWS", array("107568694359531520"));
 //                          AndrewSM64              FrostyZako              MarvJungs               Mushie64               Tomatobird8
 define("ADMIN_SITE", array("260945217664974860", "120084264489451520", "107568694359531520", "264457080889409536", "210326854282772480"));
 
@@ -9,12 +9,12 @@ define("DB_USER", "db");
 define("DB_PASS", "db");
 define("DB_NAME", "new_application");
 
-define("DISCORD_CLIENT_ID", "1176646640321441802");
-define("DISCORD_CLIENT_SECRET", "u5_sAUYf9d9w66ZZX2h1krOTUxtKiKol");
-define("DISCORD_REDIRECT_URI", "http://localhost/login/process-oauth.php");
+define("DISCORD_CLIENT_ID", "DISCORD_CLIENT_ID");
+define("DISCORD_CLIENT_SECRET", "DISCORD_CLIENT_SECRET");
+define("DISCORD_REDIRECT_URI", "https://localhost/login/process-oauth.php");
 
-define("TWITCH_CLIENT_ID", "7xxy8kh0hvswrrnnpzjpej41h5qxzz");
-define("TWITCH_CLIENT_SECRET", "26i3l38b88g131htepkjbn8t9rolon");
+define("TWITCH_CLIENT_ID", "TWITCH_CLIENT_ID");
+define("TWITCH_CLIENT_SECRET", "TWITCH_CLIENT_SECRET");
 
 error_reporting(E_ERROR);
 
